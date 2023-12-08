af6e5f42fc41d60d68b139fd395f1ce94e9d1d65
DemonRain7 <1064132526@qq.com>
2023-12-08 20:33:26 +0800

    test_db更新
    更新了一下test_db

--- work/views.py
+++ work/views.py
@@ -2,15 +2,25 @@ from django.shortcuts import render
 from .models import *
 
 
+# python manage.py makemigrations
+# python manage.py migrate
+
 # Create your views here.
 def show_work(request):
     return render(request, 'dashboard.html')
 
 
-from .models import Script
+from .models import Script, Scene
 
 
 def testdb(request):
-    scripts = Script.objects.all()
+    # 从数据库中获取数据
+    script = list(Script.objects.values())  # 替换为你的实际查询条件
+    scene = list(Scene.objects.filter(script_id=1).values())
+    # Convert the QuerySet to a list
 
-    return render(request, '../templates/test_db.html', {'scripts': scripts})
+    # Create a context dictionary with the data
+    context = {'script': script, 'scene': scene}
+
+    # 渲染模板并返回响应
+    return render(request, '../templates/test_db.html', context)
